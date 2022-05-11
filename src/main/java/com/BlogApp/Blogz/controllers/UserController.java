0b21608fde7f2536ca77f3d90c31e2e40c62a9eb
Aryan Chandna <aryanchandna02@gmail.com>
2022-05-11 16:16:12 +0530

    Completed CRUD APIs for User

--- src/main/java/com/BlogApp/Blogz/controllers/UserController.java
+++ src/main/java/com/BlogApp/Blogz/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.BlogApp.Blogz.controllers;
 
+import com.BlogApp.Blogz.payloads.ApiResponse;
 import com.BlogApp.Blogz.payloads.UserDTO;
 import com.BlogApp.Blogz.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,9 +27,31 @@ public class UserController {
     }
 
     //PUT-update user
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDto, @PathVariable Integer userId){
+        UserDTO updateUserDto = this.userService.updateUser(userDto, userId);
+        return new ResponseEntity<>(updateUserDto, HttpStatus.OK);
+
+    }
 
     //DELETE-delete user
-
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+        this.userService.deleteUser(userId);
+        return new ResponseEntity(new ApiResponse("User deleted", true), HttpStatus.OK);
+    }
     //GET-user fetch
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer userId){
+
+       UserDTO user = this.userService.getUserById(userId);
+        return new ResponseEntity(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
 
 }
