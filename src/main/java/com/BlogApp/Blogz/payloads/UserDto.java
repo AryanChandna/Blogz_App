8d391a6b933e73a9a499e368b0b7cdbdf1e4f538
Aryan Chandna <aryanchandna02@gmail.com>
2022-05-12 17:39:38 +0530

    Added validation message for email, password and username

--- src/main/java/com/BlogApp/Blogz/payloads/UserDto.java
+++ src/main/java/com/BlogApp/Blogz/payloads/UserDto.java
@@ -4,15 +4,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
     private Integer id;
-    private String name;
-    private String email;
-    private String password;
-    private String about;
 
+    @NotEmpty
+    @Size(min = 4, message ="Username should have a minimum of 4 characters")
+    private String name;
+
+    @Email(message = "Email address is not valid")
+    private String email;
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+
+    @NotEmpty
+    private String about;
 
 }
