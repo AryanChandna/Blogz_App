81626545489f4ac543d1af974a3b9629e6fcb8a1
Aryan Chandna <aryanchandna02@gmail.com>
2022-05-17 18:22:48 +0530

    Image upload with a UUID and getting the image from database

--- src/main/java/com/BlogApp/Blogz/controllers/PostController.java
+++ src/main/java/com/BlogApp/Blogz/controllers/PostController.java
@@ -1,14 +1,17 @@
 package com.BlogApp.Blogz.controllers;
 
+import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.BlogApp.Blogz.config.AppConstants;
 import com.BlogApp.Blogz.payloads.ApiResponse;
 import com.BlogApp.Blogz.payloads.PostDto;
 import com.BlogApp.Blogz.payloads.PostResponse;
+import com.BlogApp.Blogz.services.FileService;
 import com.BlogApp.Blogz.services.PostService;
 import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +37,12 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("%{project.image}")
+    private String path;
 
 
 //	create
@@ -65,7 +74,7 @@ public class PostController {
     }
     //get all posts
     @GetMapping("/posts")
-    public ResponseEntity<PostResponse> getAllPosts(@RequestParam(value = "pageNumber", defaultValue ="0",required = false)Integer pageNumber, @RequestParam(value="pageSize",defaultValue ="5",required = false) Integer pageSize,@RequestParam(value="sortBy", defaultValue ="postId", required = false)String sortBy,@RequestParam(value ="sortDir", defaultValue ="asc", required = false) String sortDir) {
+    public ResponseEntity<PostResponse> getAllPosts(@RequestParam(value = "pageNumber",defaultValue= AppConstants.PAGE_NUMBER,required = false)Integer pageNumber, @RequestParam(value="pageSize",defaultValue=AppConstants.PAGE_SIZE,required = false) Integer pageSize, @RequestParam(value="sortBy", defaultValue = AppConstants.SORT_BY, required = false)String sortBy, @RequestParam(value ="sortDir", defaultValue =AppConstants.SORT_DIR, required = false) String sortDir) {
 
         PostResponse postResponse = this.postService.getAllPosts(pageNumber, pageSize, sortBy, sortDir);
         return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
@@ -97,6 +106,27 @@ public class PostController {
 
         List<PostDto> result = this.postService.searchPosts(keyword);
         return new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
+    }
+
+    //image upload
+    @PostMapping("/post/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(@RequestParam("image") MultipartFile image, @PathVariable Integer postId) throws IOException {
+
+        PostDto postDto = this.postService.getPostById(postId);
+        String fileName = this.fileService.uploadImage(path,image);
+
+    postDto.setImageName(fileName);
+    PostDto updatedPost = this.postService.updatePost(postDto,postId);
+    return new ResponseEntity<PostDto>(updatedPost, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value="post/image/{imageName}",produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException{
+
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 
 
