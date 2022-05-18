9da777fec1d48e8483412a2e4b5b48023bcd0cdf
Aryan Chandna <aryanchandna02@gmail.com>
2022-05-18 15:56:23 +0530

    Implemented spring security and password encoder

--- src/main/java/com/BlogApp/Blogz/BlogzApplication.java
+++ src/main/java/com/BlogApp/Blogz/BlogzApplication.java
@@ -1,12 +1,18 @@
 package com.BlogApp.Blogz;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogzApplication {
+public class BlogzApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogzApplication.class, args);
@@ -18,4 +24,8 @@ public class BlogzApplication {
 		return new ModelMapper();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("nnnn"));
+	}
 }
--- src/main/java/com/BlogApp/Blogz/repositories/UserRepo.java
+++ src/main/java/com/BlogApp/Blogz/repositories/UserRepo.java
@@ -3,5 +3,9 @@ package com.BlogApp.Blogz.repositories;
 import com.BlogApp.Blogz.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
 }
